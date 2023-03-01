23f3b08d472805e0a03d9c623ce5868688a41019
gajewskikamil <gajewskikamil.poczta@gmail.com>
2023-03-01 12:08:51 +0100

    homework

--- rozdzial1/live_coding/ex11.py
+++ rozdzial1/live_coding/ex11.py
@@ -8,9 +8,18 @@ Uzupełnij kod tak, aby wykonywał określone zadania
 # będzie mniejsza od 10 - wypisz dwukrotność podanej
 # liczby, jeśli większa od 100 - wypisz jej połowę,
 # w każdym innym przypadku wypisz wpisaną wartość
+number=int(input("Podaj liczbę: "))
+if(number<10): print(2*number)
+else:
+    if(number>100): print(number/2)
+    else: print(number)
+
 
 
 # przypisz zmiennej `y` wartość 'dużo' jeśli `x`
 # jest większe od 10, w przeciwnym przypadku wartość
 # 'mało', użyj jednolinijkowej wersji instrukcji
 # warunkowej
+x=int(input("podaj x: "))
+y="dużo" if x>10 else "mało"
+print(y)
--- rozdzial1/live_coding/ex12.py
+++ rozdzial1/live_coding/ex12.py
@@ -7,3 +7,10 @@ Uzupełnij kod tak, aby wykonywał określone zadania
 # zapytaj użytkownika o nazwisko kierowcy F1, a
 # następnie wypisz odpowiadający mu zespół,
 # w przypadku braku dopasowania wypisz 'brak danych'
+drivers = {
+    "Verstappen": "RedBull",
+    "Hamilton": "Mercedes",
+}
+surename=input("podaj nazwisko kierowcy F1: ")
+if(drivers[surename]): print(drivers[surename])
+else: print("Brak Danych")
