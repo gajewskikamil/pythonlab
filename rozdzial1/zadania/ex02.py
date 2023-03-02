d1b5672313a0178aae23f00bb68757a2b2b16de3
gajewskikamil <gajewskikamil.poczta@gmail.com>
2023-03-02 22:36:27 +0100

    lekcja 3

--- rozdzial1/zadania/ex02.py
+++ rozdzial1/zadania/ex02.py
@@ -3,11 +3,20 @@ Stwórz grę, która zapyta użytkownika o wybór: 'kamień', 'papier' lub 'noż
 i wypisz wynik gry.
 """
 from random import choice
-you=int(input("1: Kamień \n2: Papier \n3: Nożyce \nWpisz liczbę"))
+gra= {
+    1:"Kamień",
+    2:"Papier",
+    3:"Nożyce"}
+you=int(input("1: Kamień \n2: Papier \n3: Nożyce \nWpisz liczbę: "))
+print("Wybrałeś: "+gra[you])
+computer=choice(range(1,3))
+print("Komputer wybrał: "+gra[computer])
+match computer:
+    case 1:
+        print("Remis")
+    case 2:
+        print("Wygrana")
+    case 3:
+        print("Przegrana")
 
-k="Kamień"
-p="Papier"
-n="Nożyce"
-computer=choice(k,p,n)
-print(you)
 #win = "Wygrałeś" if
